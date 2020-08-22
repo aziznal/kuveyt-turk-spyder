8841e51284dc784f7049ae080dc540a2e08aac37
aziznal <aziznal.dev@gmail.com>
2020-08-22 10:52:46 +0300

    Update BankSpyder

--- BankSpyder.py
+++ BankSpyder.py
@@ -1,16 +1,6 @@
-from datetime import datetime
-import json
-
 from BaseSpyder import BaseSpyder
 
 
-# TODO: write the following in docs:
-#   After Inheriting this class, all you need to do is define the two abstract methods,
-#   making sure to return the values in the proper structure. the rest is handled by
-#   the filter_data method
-
 class BankSpyder(BaseSpyder):
-    ___metaclass__ = ABCMeta
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
